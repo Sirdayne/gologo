f573c2883f974ae30f03a809bd36ace47ba6cdf3
LFY <mussinyernar@gmail.com>
2016-11-21 15:46:43 +0600

    new changes

--- js/gologo.js
+++ js/gologo.js
@@ -13,6 +13,16 @@ $(document).ready(function(){
         $('.driver-stories-btn-2').toggleClass('dr-st-active');
     });
     
+    $('.frm-js').click(function() {
+        $('.frm-js').removeClass('frm-active');
+        $(this).addClass('frm-active');
+    });
+    
+    $('.frm-next').click(function() {
+        $('.header-form-left').show();
+        $('.frm-next').addClass('frm-anima');
+    });
+    
     var telephone = $('.telephone');
     var marginLeft = $('.telephone-2 img').css('margin-left');
     var height = $('.telephone-2 img').css('height');
@@ -36,32 +46,43 @@ $(document).ready(function(){
     $('.js-inner').css('height', heightT );
     $('.js-inner').css('height', '-=30');
     
-    $('.main-tel').waypoint({
-        handler: function(direction) {
-            if (direction === 'down') {
-                $('.tel-fixed').show();
-                //$('.telephone').addClass('tel-fixed');
-                //$('.tel-img').css('margin-left' , marginLeft );
-                //$('.telephone-3 img').css('visibility' , 'hidden' );
-            } 
-            else {
-                $('.tel-fixed').hide();
-                //$('.telephone').removeClass('tel-fixed');
-                //$('.telephone-3 img').css('visibility' , 'visible' );
-            }
-        },
-        offset: 0
-    });
-    
+    if ($(window).width() > 1000) {  
+        $('.main-tel').waypoint({
+            handler: function(direction) {
+                if (direction === 'down') {
+                    $('.tel-fixed').show();
+                    
+                    $('.tel-ch').removeClass('slideInUp').removeClass('slideOutUp');
+                    
+                    //$('.telephone').addClass('tel-fixed');
+                    //$('.tel-img').css('margin-left' , marginLeft );
+                    //$('.telephone-3 img').css('visibility' , 'hidden' );
+                } 
+                else {
+                    $('.tel-fixed').hide();
+                    
+                    $('.tel-ch').removeClass('slideInUp').removeClass('slideOutUp');
+                    //$('.telephone').removeClass('tel-fixed');
+                    //$('.telephone-3 img').css('visibility' , 'visible' );
+                }
+            },
+            offset: 0
+        });
+   
+
     $('#tel-right').waypoint({
         handler: function(direction) {
             if (direction === 'down') {
                 $('.tel-fixed').hide();
+                
+                $('.tel-ch').removeClass('slideInUp').removeClass('slideOutUp');
                 //$('.telephone').removeClass('tel-fixed');
                 //$('.telephone-3 img').css('visibility' , 'visible' );
             } 
             else {
                 $('.tel-fixed').show();
+                
+                $('.tel-ch').removeClass('slideInUp').removeClass('slideOutUp');
                 //$('.telephone').addClass('tel-fixed');
                 //$('.tel-img').css('margin-left' , marginLeft );
                 //$('.telephone-3 img').css('visibility' , 'hidden' );
@@ -69,16 +90,24 @@ $(document).ready(function(){
         },
         offset: 0
     });
-    
+
     $('.telephone-2').waypoint({
         handler: function(direction) {
             if (direction === 'down') {
-                $('.tel-ch').hide();
+                //$('.tel-ch').hide();
+                $('#tel-ch-3').hide();
                 $('#tel-ch-2').show();
+                
+                
+                $('#tel-ch-1').removeClass('slideInUp').addClass('slideOutUp').css('z-index','1');
+                $('#tel-ch-2').removeClass('slideOutUp').addClass('slideInUp').css('z-index','2');
             } 
             else {
-                $('.tel-ch').hide();
+                //$('.tel-ch').hide();
                 $('#tel-ch-1').show();
+                
+                $('#tel-ch-1').removeClass('slideOutUp').addClass('slideInUp').css('z-index','2');
+                $('#tel-ch-2').removeClass('slideInUp').addClass('slideOutUp').css('z-index','1');
             }
         },
         offset: 200
@@ -87,16 +116,23 @@ $(document).ready(function(){
     $('.telephone-3').waypoint({
         handler: function(direction) {
             if (direction === 'down') {
-                $('.tel-ch').hide();
+                //$('.tel-ch').hide();
                 $('#tel-ch-3').show();
+                $('#tel-ch-1').hide();
+                
+                $('#tel-ch-2').removeClass('slideInUp').addClass('slideOutUp').css('z-index','1');
+                $('#tel-ch-3').removeClass('slideOutUp').addClass('slideInUp').css('z-index','2');
             } 
             else {
-                $('.tel-ch').hide();
-                $('#tel-ch-2').show();
+                //$('.tel-ch').hide();
+                
+                $('#tel-ch-2').removeClass('slideOutUp').addClass('slideInUp').css('z-index','2');
+                $('#tel-ch-3').removeClass('slideInUp').addClass('slideOutUp').css('z-index','1');
             }
         },
         offset: 200
     });
-    
+        
+    };
 });
 
